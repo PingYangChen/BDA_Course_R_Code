408ab0102009a1778621b5e3727b83f36bc385e1
PingYangChen <pychen.ping@gmail.com>
2025-03-09 20:03:09 +0800

    add lotto data

--- AssociationRule.R
+++ AssociationRule.R
@@ -12,6 +12,7 @@ itemFrequencyPlot(Groceries, topN = 20)
 
 
 
+
 rules <- apriori(Groceries, 
   parameter = list(
     support = 0.001,   # minimal support of an item set
@@ -43,3 +44,47 @@ plot(rules.sub, method = "graph")
 plot(rules, method="grouped", limit = 20)
 
 plot(rules, method = "matrix", limit = 6)
+
+
+
+
+
+
+df <- read.csv('sample_data/lotto_2023-202503037.csv')
+names(df)
+
+SplitNumbers <- function(num_list) {
+  tmp <- strsplit(num_list, ",")[[1]]
+  n_num <- length(tmp)
+  out <- as.integer(c(strsplit(tmp[1], "\\[")[[1]][2],
+                      tmp[2:(n_num-1)],
+                      strsplit(tmp[n_num], "\\]")[[1]][1]))
+  return(out)
+}
+
+sn <- tid <- c()
+for (i in 1:nrow(df)) {
+  stmp <- SplitNumbers(df$獎號[i])
+  sn <- c(sn, stmp)
+  tid <- c(tid, rep(df$期別[i], length(stmp)))
+}
+
+df_splitted <- data.frame(
+  tid = tid,
+  item = sn
+)
+
+
+adf <- as(split(df_splitted$item, df_splitted$tid), "transactions")
+head(DATAFRAME(adf), 12)
+inspect(adf)
+
+
+rules <- apriori(adf, 
+                 parameter = list(
+                   support = 0.001,   # minimal support of an item set
+                   confidence = 0.5, # minimal confidence of rules/association hyperedges
+                   minlen = 1 # minimal number of items per item set
+                 ))
+# Display the top 3 support rules
+inspect(head(rules, n = 5, by = "support"))
